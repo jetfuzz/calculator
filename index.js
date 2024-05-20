985105e60e47dc548611d9f41e3c8e9228d119e2
jetfuzz <1jsf23@gmail.com>
2024-05-20 12:50:38 -0500

    Create functions for basic operators

---
+++ index.js
@@ -0,0 +1,33 @@
+//Create functions for all basic math operators
+//(addition, subtraction, multiply and divide)
+
+//Create variables for each aspect of calculation
+
+let firstNum;
+let secondNum;
+let operator;
+
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract(a, b) {
+    let sum = a - b;
+    return sum;
+}
+
+function multiply(a, b) {
+    let sum = a * b;
+    return sum;
+}
+
+function divide(a, b) {
+    let sum = a / b;
+    return sum;
+}
+
+console.log(add(3,5))
+console.log(subtract(3,5))
+console.log(multiply(3,5))
+console.log(divide(3,5))
