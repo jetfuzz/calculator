f10475bac61453474ad4718f01d1cd0b4a0f694c
jetfuzz <1jsf23@gmail.com>
2024-05-25 10:42:49 -0500

    make calculator work with basic math operators
    remove unnecessary functions and add to operator button function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let display = document.getElementById("calculator-display");
 let numBtn = document.querySelectorAll(".num-btn");
 let clearBtn = document.getElementById("clear-btn");
-let equalsBtn = document.getElementById("equals-btn");
 let operatorBtn = document.querySelectorAll(".operator-btn");
 
 let firstNum = "";
@@ -10,66 +9,53 @@ let operator = "";
 
 display.innerText = 0;
 
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(firstNum, secondNum, operator) {
-    firstNum = parseFloat(firstNum);
-    secondNum = parseFloat(secondNum);
-
-    if (operator === "+") {
-        return firstNum + secondNum
-    } else if (operator === "-") {
-        return firstNum - secondNum
-    } else if (operator === "&times") {
-        return firstNum * secondNum
-    } else if (operator === "÷") {
-        return firstNum / secondNum
-    } else return "lmao"
-}
-
-//Display numbers on button click
-numBtn.forEach(button => button.addEventListener("click", () => {
-    let buttonContent = button.textContent;
-    display.textContent = buttonContent;
-}))
-
 numBtn.forEach(number => {
     number.addEventListener("click", e => {
+    let numberContent = number.textContent;
+    display.textContent = numberContent;
         if (operator === "") {
             firstNum += e.target.innerText;
-            console.log(firstNum)
         } else {
             secondNum += e.target.innerText;
-            console.log(secondNum)
         }
     })
 })
 
 operatorBtn.forEach(op => {
     op.addEventListener("click", e => {
-        operator = e.target.innerText;
-        console.log(operator);
+        if (e.target.innerText !== "=") {
+            operator = e.target.innerText;
+
+            console.log(firstNum);
+            console.log(operator);
+
+        } else {
+            console.log(secondNum);
+
+            switch (operator) {
+                case "+":
+                    console.log(parseFloat(firstNum) + parseFloat(secondNum));
+                    display.textContent = parseFloat(firstNum) + parseFloat(secondNum);
+                    break;
+
+                case "-":
+                    console.log(parseFloat(firstNum) - parseFloat(secondNum));
+                    display.textContent = parseFloat(firstNum) - parseFloat(secondNum);
+                    break;
+                case "×":
+                    console.log(parseFloat(firstNum) * parseFloat(secondNum));
+                    display.textContent = parseFloat(firstNum) * parseFloat(secondNum);
+                    break;
+                case "÷":
+                    console.log(parseFloat(firstNum) / parseFloat(secondNum));
+                    display.textContent = parseFloat(firstNum) / parseFloat(secondNum);
+                    break;
+                default:
+                    break;
+            }
+        }
     });
 });
-
-equalsBtn.addEventListener("click", () => {
-    console.log(operate(firstNum, secondNum, operator));
-    display.textContent = operate(firstNum, secondNum, operator);
-})
 
 clearBtn.addEventListener("click", () => {
     firstNum = "";
